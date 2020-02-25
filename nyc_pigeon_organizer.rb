fac5e823848f0161483adc06881ab259ae2b4b30
Brett Bedevian <brettbedevian@gmail.com>
2020-02-25 18:02:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+pigeon_list = {}
+  
+data.each_pair do |key, value| 
+  value.each_pair do |inner_value, name| 
+    name.each do |name|
+    if !pigeon_list[name]
+      pigeon_list[name] = {}
+    end 
+    
+    if !pigeon_list[name][key]
+      pigeon_list[name][key] = []
+    end 
+    
+    pigeon_list[name][key] << inner_value.to_s 
+end 
+end 
+end 
+pigeon_list
+end 
